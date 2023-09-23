f5522c66e80e41bf65863785504ed548f935c552
migsv <106558565+migsv@users.noreply.github.com>
2023-09-22 23:42:33 -0300

    Update Projeto 2 - Calculadora.py

--- Projeto 2 - Calculadora.py
+++ Projeto 2 - Calculadora.py
@@ -1,44 +1,62 @@
 calculadora = 0
 
 while True:
-    num1 = input('Informe o primeiro valor: ')
-    
-    if num1 == '':
-        break
-    numero1 = float(num1)
-
     print('''
+    Calculadora
+    ===============================
     [+] Soma
     [-] Subtração
     [*] Multiplicação
     [/] Divisão
+    ===============================
+    [x] Limpar calculadora
+    [ENTER] Encerra o programa
+    ===============================
     ''')
 
-    escolha = str(input('Digite o numero da operação que voce quer: '))
+    escolha = input('Digite o sinal da operação que você quer usar: ').lower()
 
     if escolha == '':
         break
 
-    print()
+    if escolha == 'x':
+        calculadora = 0
+        continue
+
+    if calculadora == 0:
+        num1 = input('Informe o primeiro valor: ')
+
+        if num1 == '':
+            print('Processo finalizado.')
+            break
+
+        numero1 = float(num1)
+
+    else:
+        numero1 = calculadora
 
     num2 = input('Informe o segundo valor: ')
 
     if num2 == '':
+        print('Processo finalizado.')
         break
 
     numero2 = float(num2)
 
     if escolha == '+':
         calculadora = numero1 + numero2
-        print(calculadora)
+        print(f'O resultado foi: {calculadora}')
     elif escolha == '-':
         calculadora = numero1 - numero2
-        print(calculadora)
+        print(f'O resultado foi: {calculadora}')
     elif escolha == '*':
         calculadora = numero1 * numero2
-        print(calculadora)
+        print(f'O resultado foi: {calculadora}')
     elif escolha == '/':
-        calculadora = numero1 / numero2
-        print(calculadora)
+        if numero2 == 0:
+            print('Não é possível dividir por zero.')
+        else:
+            calculadora = numero1 / numero2
+            print(f'O resultado foi: {calculadora}')
     else:
-        break
+        print('Algo foi digitado de forma errada. Digite novamente!')
