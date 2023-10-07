749698fca92b3138b56289a3817f421613d0cd1c
Ewerton Peixoto de Alencar Arrais <142873014+ewerside@users.noreply.github.com>
2023-10-07 15:55:22 -0300

    Add files via upload

--- Projeto 1 - Simulador de Dados.py
+++ Projeto 1 - Simulador de Dados.py
@@ -9,34 +9,39 @@ def numero_valido(x):
 
 def rolar_dados():
     while True:
-        lados = input("Digite o número de lados do(s) dado(s): ")
+        lados = input("Digite o número de lados do(s) dado(s). (Ou pressione ENTER para sair.) ")
 
         if lados == '':
             print('Programa finalizado')
             break
 
         if not numero_valido(lados):
-            print('Número inválido!')
+            print('Por favor, digite um número maior que zero.')
             continue
 
         num_lados = int(lados)
 
-        num_rolagens = input('Quantos dados você deseja rolar? ')
+        while True:
+            dados = input('Quantos dados você deseja rolar? (Pressione ENTER para rolar apenas 1.) ')
 
-        if num_rolagens == '':
-            num_rolagens = 1
-        elif not numero_valido(num_rolagens):
-            print('Número inválido!')
-            continue
+            if dados == '':
+                dados = 1
+            elif not numero_valido(dados):
+                print('Por favor, digite um número maior que zero.')
+                continue
+            break
 
-        dados = int(num_rolagens)
+        num_rolagens = int(dados)
+
+        valores_rolados = []
 
         i = 0
-        for dado in range(1, dados + 1):
+        for dado in range(1, num_rolagens + 1):
             i += 1
             rolagem = randint(1, num_lados)
             print(f'Rolagem {i}: {rolagem}')
+            valores_rolados.append(rolagem)
 
-        print('')
+        print(f'{num_rolagens} dado(s) de {num_lados} lado(s): {valores_rolados}\n')
 
 rolar_dados()
